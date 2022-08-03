b26d96ea523f1aeaae95d949eae37c1e5fc280e1
smartbrein <52868184+sbpythonista@users.noreply.github.com>
2022-08-03 11:42:16 +0100

    generateByUpload

---
+++ dist/contact.4f492e9b.js
@@ -0,0 +1,13 @@
+//Dark & Light Mode
+document.getElementById("darkmode").onclick = function() {
+    darkMode();
+};
+function darkMode() {
+    var dark = document.body;
+    dark.classList.toggle("dark-mode");
+} // // document.getElementById("darkmode").onclick = function() {darkMode()};
+ // function myFunction() {
+ //   document.body.style.backgroundColor = "red";
+ // }
+
+//# sourceMappingURL=contact.4f492e9b.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,3 +14,15 @@ function darkMode() {
 // function myFunction() {
 //   document.body.style.backgroundColor = "red";
 // }
+
+
+//  popup for upload images 
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+
+function hello(){  
+    alert("hello javatpoint user");  
+}
